a6af0a95131764ed9651ad89c82c451891645839
Nguyen Tai Kiet <kiet.nt1494@sinhvien.hoasen.edu.vn>
2023-10-31 13:12:47 +0700

    home, quote, emoji

--- Client/GuessTheChampClient/App.js
+++ Client/GuessTheChampClient/App.js
@@ -1,20 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import React, { Component } from 'react';
+import Home from './Components/HomeComponent'
+import Quote from './Components/QuoteComponent'
+import Emoji from './Components/EmojiComponent';
+import { createStackNavigator } from "@react-navigation/stack";
 
-export default function App() {
+class App extends Component {
+  // render() {
+  //   return (
+  //     <Quote />
+  //   );
+  // }
+  render(){
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Quote/>
+    </NavigationContainer>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  }
+  }
+export default App; 
